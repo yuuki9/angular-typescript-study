5ed52645ff6d863d9ce7594a65ef016873dbf45a
진호성 <yuuki@newlayer.kr>
2023-02-23 16:45:03 +0900

    코드 수정

--- threejs-angular-sample/src/app/Basic/angularthreejs/Other/ElectricTunnel.d.ts
+++
@@ -1,5 +0,0 @@
-import * as THREE from 'three';
-export declare class Models {
-    constructor();
-    static MakeTunnel(radius: number, height: number, tunnelTexturePath: string, planeTexturePath: string): THREE.Object3D;
-}
--- threejs-angular-sample/src/app/Basic/angularthreejs/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./ThreeJsLib";
-export * from "./Struct";
-export * from "./Utils";
-export * as THREE from 'three';
